3a75a10111387abdf2c9adef2960003716395065
OldSociety <headmistrust@outlook.com>
2022-04-29 15:26:49 -0700

    window and scrollbar

---
+++ src/Components/StudentProfile.jsx
@@ -0,0 +1,83 @@
+import React, { useState, useEffect } from 'react'
+
+const StudentProfile = () => {
+  const [fetchedData, setFetchedData] = useState(null)
+  const [pageError, setPageError] = useState(null)
+
+  useEffect(() => {
+    //set up with api call
+    function fetchData() {
+      const abortController = new AbortController()
+      setPageError(null)
+
+      fetch('https://api.hatchways.io/assessment/students')
+        .then((response) => response.json())
+        .then(setFetchedData)
+        .catch(setPageError)
+
+      return () => abortController.abort(pageError)
+    }
+    fetchData()
+  }, [])
+
+  if (fetchedData) {
+    let { students } = fetchedData
+    return (
+      <div className="row">
+        <div className="container">
+          <div className="col-12">
+            <div className="row">
+              {students.map((student, index) => {
+                let {
+                  city,
+                  company,
+                  email,
+                  firstName,
+                  grades,
+                  id,
+                  lastName,
+                  pic,
+                  skill,
+                } = student //deconstruct student object
+
+                function findAverage(grades) {
+                  //get average of grades for each student
+                  let average = grades.reduce(
+                    (start, end, index, grades) => start + end / grades.length,
+                    0
+                  )
+                  return average
+                }
+
+                return (
+                  <ul>
+                    <li key={index}>
+                      <div className="profile">
+                        <img
+                          className="icon"
+                          src={`${pic}`}
+                          alt={`${firstName}`}
+                        />
+                        <div className="name fw-bold fs-1">
+                          {`${firstName.toUpperCase()} ${lastName.toUpperCase()}`}
+                        </div>
+                        <div className="">{`Email: ${email}`}</div>
+                        <div className="">{`Company: ${company}`}</div>
+                        <div className="">{`Skill: ${skill}`}</div>
+                        <div className="">
+                          {`Average: ${findAverage(grades)}`}
+                        </div>
+                      </div>
+                    </li>
+                  </ul>
+                )
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default StudentProfile
--- src/Core/Home.jsx
+++ src/Core/Home.jsx
@@ -1,77 +1,12 @@
-import React, { useState, useEffect } from 'react'
-// import { listStudents } from '../utils/api';
+import React from 'react'
+import StudentProfile from '../Components/StudentProfile'
 
 const Home = () => {
-  const [fetchedData, setFetchedData] = useState(null)
-  const [pageError, setPageError] = useState(null)
-
-  useEffect(() => { //set up with api call
-    function fetchData() {
-      const abortController = new AbortController()
-      setPageError(null)
-
-      fetch('https://api.hatchways.io/assessment/students') 
-        .then((response) => response.json())
-        .then(setFetchedData)
-        .catch(setPageError)
-
-      return () => abortController.abort(pageError)
-    }
-    fetchData()
-  }, [])
-
-  if (fetchedData) {
-    let { students } = fetchedData
-    return (
-      <div className="row">
-        <div className="container">
-          <div className="col-12">
-            <div className="row">
-              {students.map((student, index) => {
-                let {
-                  city,
-                  company,
-                  email,
-                  firstName,
-                  grades,
-                  id,
-                  lastName,
-                  pic,
-                  skill,
-                } = student //deconstruct student object
-
-                function findAverage(grades) {
-                  //get average of grades for each student
-                  let average = grades.reduce(
-                    (start, end, index, grades) => start + end / grades.length,
-                    0
-                  )
-                  return average
-                }
-
-                return (
-                  <ul>
-                    <li key={index}>
-                      <img className="icon" src={`${pic}`} alt={`${firstName}`} />
-                      <div className="name fw-bold fs-1">
-                        {`${firstName.toUpperCase()} ${lastName.toUpperCase()}`}
-                      </div>
-                      <div className="">{`Email: ${email}`}</div>
-                      <div className="">{`Company: ${company}`}</div>
-                      <div className="">{`Skill: ${skill}`}</div>
-                      <div className="">
-                        {`Average: ${findAverage(grades)}`}
-                      </div>
-                    </li>
-                  </ul>
-                )
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+  return (
+    <div className='studentProfiles container-fluid'>
+      <StudentProfile />
+    </div>
+  )
 }
 
 export default Home
